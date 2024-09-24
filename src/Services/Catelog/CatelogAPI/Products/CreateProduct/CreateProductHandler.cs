c4cddffab8e394510432b1e647d5e0488ccaf687
MadhuADV <madhu.pandit@advitiyasolutions.com>
2024-09-24 10:54:23 +0530

    Logging Behaviour is completed.

--- src/Services/Catelog/CatelogAPI/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catelog/CatelogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -14,8 +14,7 @@ public class CreateProductCommandValidator : AbstractValidator<CreateProductComm
     }
 }
 internal class CreateProductCommandHandler
-    (IDocumentSession session, IValidator<CreateProductCommand> validator
-    ) 
+    (IDocumentSession session) 
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -24,12 +23,14 @@ internal class CreateProductCommandHandler
         //Save to Database
         //return CreateProdctResut result
 
-        var result = await validator.ValidateAsync(command, cancellationToken);
-        var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
-        if (errors.Any())
-        {
-            throw new ValidationException(errors.FirstOrDefault());
-        }
+        //var result = await validator.ValidateAsync(command, cancellationToken);
+        //var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
+        //if (errors.Any())
+        //{
+        //    throw new ValidationException(errors.FirstOrDefault());
+        //} 
+
+       
 
         var product = new Product
         {
